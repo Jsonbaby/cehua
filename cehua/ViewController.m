705c1b727bacdd9823c4790aa5e6a2eb68a43678
张璠 <ttxs_code@163.com>
2016-08-22 14:01:29 +0800

    Initial Commit

---
+++ cehua/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  cehua
+//
+//  Created by tao wai on 16/8/22.
+//  Copyright © 2016年 jixuejiyong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ cehua/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cehua
+//
+//  Created by tao wai on 16/8/22.
+//  Copyright © 2016年 jixuejiyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
